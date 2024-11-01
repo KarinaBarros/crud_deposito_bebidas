c04cd3fdeee39229a4f50ec36e508a00ff8cbb83
Karina Barros <barros_kari@hotmail.com>
2024-11-01 16:44:11 -0300

    açoes do estoque

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -2,7 +2,7 @@ const token = localStorage.getItem('token');
 const id = localStorage.getItem('id');
 //Função para verificar rota protegida
 async function verificarToken() {
-    if(token){
+    if (token) {
         try {
             const response = await axios.get('/api/protected', {
                 headers: {
@@ -14,46 +14,196 @@ async function verificarToken() {
             console.error('Erro:', error);
             window.location.href = '/login';
         }
-    }else{
+    } else {
         window.location.href = '/login';
     }
 }
 verificarToken();
 
-//Buscar estoque
+
+//funçao para popular tabela de estoque
+let idProdutoSelecionado;
 async function buscaEstoque() {
-    try{
-        const response = await axios.get('/api/estoque', {
+    if (id && token) {
+        try {
+            const response = await axios.post('/api/estoque', { user_id: id }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            let tbodyEstoque = document.getElementById('tbody-estoque');
+            tbodyEstoque.innerHTML = '';
+
+            response.data.forEach(function (item) {
+                let novaLinha = document.createElement("tr");
+
+                let tipo = document.createElement("td");
+                tipo.textContent = item.tipo;
+                novaLinha.appendChild(tipo);
+
+                let nome = document.createElement("td");
+                nome.textContent = item.nome;
+                novaLinha.appendChild(nome);
+
+                let unidades = document.createElement("td");
+                unidades.textContent = item.total_unidades;
+                novaLinha.appendChild(unidades);
+
+                let tdbotao = document.createElement("td");
+
+                let botao = document.createElement("button");
+                botao.classList.add('btn-alterar-estoque');
+                botao.title = "Altere dados do produto";
+                botao.innerHTML = `<i class="fas fa-edit"></i>`;
+                botao.onclick = () => EstoqueId(item.id);
+
+                let botaoExcluir = document.createElement("button");
+                botaoExcluir.classList.add('btn-alterar-estoque');
+                botaoExcluir.title = "Excluir produto";
+                botaoExcluir.innerHTML = `<i class="fas fa-trash"></i>`;
+                botaoExcluir.onclick = () => IdExcluir(item.id);
+
+                // Defina um evento de clique que chama EstoqueId com o ID do item
+
+                tdbotao.appendChild(botao);
+                tdbotao.appendChild(botaoExcluir);
+                novaLinha.appendChild(tdbotao);
+
+
+                tbodyEstoque.appendChild(novaLinha);
+            });
+
+        } catch (error) {
+            console.error('Erro:', error);
+        }
+    } else {
+        window.location.href = '/login';
+    }
+
+}
+
+//Buscar dados para alterar estoque
+
+async function EstoqueId(idProduto) {
+    try {
+        const response = await axios.post('/api/estoque-id', { id: idProduto }, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
-        })
-        let tbodyEstoque = document.getElementById('tbody-estoque');
-        response.data.forEach(function(item) {
-            let novaLinha = document.createElement("tr");
+        });
+        idProdutoSelecionado = idProduto;
+        console.log("produto", idProdutoSelecionado);
+        console.log(response.data);
 
-            let tipo = document.createElement("td");
-            tipo.textContent = item.tipo;
-            novaLinha.appendChild(tipo);
-            let nome = document.createElement("td");
-            nome.textContent = item.nome;
-            novaLinha.appendChild(nome);
-            let unidades = document.createElement("td");
-            unidades.textContent = item.total_unidades;
-            novaLinha.appendChild(unidades);
+        // Atualiza o conteúdo do nome do produto
+        const nomeProduto = document.getElementById('nome-produto');
+        nomeProduto.textContent = "Bebida: " + response.data[0].nome;
 
-            tbodyEstoque.appendChild(novaLinha);
-        })
-        
-    }catch(error){
+        const NovoValor = document.getElementById("novo-venda-valor");
+        NovoValor.value = response.data[0].valor_venda_por_unidade;
+        formatarDinheiro(NovoValor);
+
+        // Exibe e oculta os containers de estoque
+        const estoquePrincipal = document.getElementById("container-estoque-principal");
+        const alterarEstoque = document.getElementById("container-alterar-estoque");
+        if (estoquePrincipal && alterarEstoque) {
+            estoquePrincipal.style.display = 'none';
+            alterarEstoque.style.display = 'block';
+        }
+
+    } catch (error) {
         console.error('Erro:', error);
     }
 }
+
 buscaEstoque();
+
+//pesquisar no estoque
+const searchInput = document.getElementById("pesquisa-estoque");
+const tableBody = document.getElementById("tbody-estoque");
+
+searchInput.addEventListener("input", function () {
+    const query = this.value.toLowerCase(); // Captura o texto do input em minúsculas
+    const rows = tableBody.getElementsByTagName("tr"); // Seleciona todas as linhas do tbody específico de estoque
+
+    for (let row of rows) {
+        const nomeCell = row.cells[1]; // Seleciona a segunda coluna (index 1) que contém o nome do produto
+        const itemText = nomeCell ? nomeCell.textContent.toLowerCase() : ""; // Conteúdo do <td> em minúsculas
+
+        // Exibe a linha se o nome do produto incluir o valor da pesquisa; caso contrário, esconde.
+        if (itemText.includes(query)) {
+            row.style.display = ""; // Exibe a linha
+        } else {
+            row.style.display = "none"; // Oculta a linha
+        }
+    }
+});
+
+//voltar para estoque principal
+function CloseAlterarEstoque() {
+    const estoquePrincipal = document.getElementById("container-estoque-principal");
+    const alterarEstoque = document.getElementById("container-alterar-estoque");
+    estoquePrincipal.style.display = 'block';
+    alterarEstoque.style.display = 'none';
+}
+
+//submeter formulário de alteração de estoque
+async function submitFormAlterar(event) {
+    event.preventDefault();
+    if (token) {
+        const formData = {
+            id: idProdutoSelecionado,
+            quantidade_caixas: document.getElementById("novo-unidade-caixa").value,
+            unidades_por_caixa: document.getElementById("novo-unidade").value,
+            valor_gasto_por_caixa: extrairValor(document.getElementById("novo-gasto-caixa").value),
+            valor_venda_por_unidade: extrairValor(document.getElementById("novo-venda-valor").value),
+        };
+        console.log(formData);
+
+        try {
+            const response = await axios.post('/api/alterar-estoque', formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            alert(response.data.message);
+            buscaEstoque();
+            document.getElementById("form-cadastro-alterar").reset();
+        } catch (error) {
+            console.error('Erro ao enviar os dados:', error);
+            alert(error.message);
+        }
+    } else {
+        window.location.href = '/login'
+    }
+}
+
+async function IdExcluir(idProdutoExcluir) {
+    if (token) {
+        let resposta = confirm("Tem certeza que deseja excluir esse item?");
+        if (resposta) {
+            try {
+                const response = await axios.post('/api/excluir-bebida', { id: idProdutoExcluir }, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                alert(response.data.message);
+                buscaEstoque();
+            } catch (error) {
+                console.error('Erro ao enviar os dados:', error);
+                alert(error.message);
+            }
+        }
+    } else {
+        window.location.href = '/login';
+    }
+}
 
 //Formataçao dinheiro
 function formatarDinheiro(input) {
-   
+
     let valor = input.value.replace(/\D/g, '');
     valor = (Number(valor) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     input.value = valor;
@@ -70,13 +220,13 @@ function extrairValor(moeda) {
 
 //nome do usuario
 const nomeStorage = localStorage.getItem('nome');
-if(nomeStorage){
+if (nomeStorage) {
     document.getElementById('nome-login').innerHTML = nomeStorage;
-}  
+}
 
 
 //Alerta sair da conta
-function alertLogout(){
+function alertLogout() {
     if (confirm('Tem certeza que deseja sair da sua conta?')) {
         localStorage.removeItem('nome');
         localStorage.removeItem('token');
@@ -95,29 +245,29 @@ var containerPendente = document.getElementById('container-pendente');
 var containerEfetuado = document.getElementById('container-efetuado');
 var containerBusca = document.getElementById('container-busca');
 var containerTVendas = document.getElementById('container-tvendas');
-var allContainers = [containerPrincipal, containerCadastro, containerVendas, containerEstoque, containerPagamento, containerPendente, containerEfetuado, containerBusca,containerTVendas];
+var allContainers = [containerPrincipal, containerCadastro, containerVendas, containerEstoque, containerPagamento, containerPendente, containerEfetuado, containerBusca, containerTVendas];
 
-var botaoCBebidas= document.getElementById('c-bebidas');
-var botaoVBebidas= document.getElementById('v-bebidas');
-var botaoPagto= document.getElementById('pagto');
-var botaoPagtoP= document.getElementById('pagto-p');
-var botaoPagtoE= document.getElementById('pagto-e');
-var botaoBuscaC= document.getElementById('busca-c');
-var botaoTodasV= document.getElementById('todas-v');
-var botaoEstoqueB= document.getElementById('estoque-b'); 
-var allBotao= [botaoCBebidas, botaoVBebidas, botaoPagto, botaoPagtoP, botaoPagtoE, botaoBuscaC, botaoTodasV, botaoEstoqueB];
+var botaoCBebidas = document.getElementById('c-bebidas');
+var botaoVBebidas = document.getElementById('v-bebidas');
+var botaoPagto = document.getElementById('pagto');
+var botaoPagtoP = document.getElementById('pagto-p');
+var botaoPagtoE = document.getElementById('pagto-e');
+var botaoBuscaC = document.getElementById('busca-c');
+var botaoTodasV = document.getElementById('todas-v');
+var botaoEstoqueB = document.getElementById('estoque-b');
+var allBotao = [botaoCBebidas, botaoVBebidas, botaoPagto, botaoPagtoP, botaoPagtoE, botaoBuscaC, botaoTodasV, botaoEstoqueB];
 
-var hamburgerMenu= document.getElementById('hamburger-menu');
-var closeMenu= document.getElementById('close-menu');
-var navigationHome= document.getElementById('navigation-home');
+var hamburgerMenu = document.getElementById('hamburger-menu');
+var closeMenu = document.getElementById('close-menu');
+var navigationHome = document.getElementById('navigation-home');
 
-function toggleNav(){
+function toggleNav() {
     navigationHome.style.display = 'block';
     hamburgerMenu.style.display = 'none';
     closeMenu.style.display = 'block';
 }
 
-function closeNav(){
+function closeNav() {
     navigationHome.style.display = 'none';
     hamburgerMenu.style.display = 'block';
     closeMenu.style.display = 'none';
@@ -129,43 +279,43 @@ function closeNav2() {
     }
 }
 
-function Close(){
+function Close() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerPrincipal.style.display = 'block';
 
-    for (var ib = 0; ib < allBotao.length; ib++){
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
 }
 
-function cadastroBebidas(){   
+function cadastroBebidas() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerCadastro.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
     botaoCBebidas.style.color = 'var(--color-highlight)';
     botaoCBebidas.style.borderBottom = '2px solid var(--color-highlight)';
-   
+
     closeNav2();
 }
 
-function vendasBebidas(){
+function vendasBebidas() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerVendas.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -175,14 +325,14 @@ function vendasBebidas(){
     closeNav2();
 }
 
-function Estoque(){
+function Estoque() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerEstoque.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -192,14 +342,14 @@ function Estoque(){
     closeNav2();
 }
 
-function Pagamento(){
+function Pagamento() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerPagamento.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -209,14 +359,14 @@ function Pagamento(){
     closeNav2();
 }
 
-function Pendente(){
+function Pendente() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerPendente.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -226,14 +376,14 @@ function Pendente(){
     closeNav2();
 }
 
-function Efetuado(){
+function Efetuado() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerEfetuado.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -242,14 +392,14 @@ function Efetuado(){
     closeNav2();
 }
 
-function Busca(){
+function Busca() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerBusca.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -258,14 +408,14 @@ function Busca(){
     closeNav2();
 }
 
-function TVendas(){
+function TVendas() {
     for (var i = 0; i < allContainers.length; i++) {
         allContainers[i].style.display = 'none';
     }
     containerTVendas.style.display = 'block';
 
-   
-    for (var ib = 0; ib < allBotao.length; ib++){
+
+    for (var ib = 0; ib < allBotao.length; ib++) {
         allBotao[ib].style.color = 'var(--color-text)';
         allBotao[ib].style.borderBottom = '2px solid var(--color-text)';
     }
@@ -278,18 +428,18 @@ function TVendas(){
 async function submitForm(event) {
     event.preventDefault();
     //user_id, nome, tipo, quantidade_caixas, unidades_por_caixa, valor_gasto_por_caixa, valor_venda_por_unidade
-    if(token && id){
+    if (token && id) {
         const formData = {
             user_id: id,
             nome: document.getElementById("nome-bebida").value,
             tipo: document.getElementById("tipo-bebida").value,
-            quantidade_caixas: document. getElementById("unidade-caixa").value,
+            quantidade_caixas: document.getElementById("unidade-caixa").value,
             unidades_por_caixa: document.getElementById("unidade").value,
             valor_gasto_por_caixa: extrairValor(document.getElementById("gasto-caixa").value),
             valor_venda_por_unidade: extrairValor(document.getElementById("venda-unidade").value),
         };
-    
-        try{
+
+        try {
             const response = await axios.post('/api/cadastro-bebidas', formData, {
                 headers: {
                     Authorization: `Bearer ${token}`
@@ -298,17 +448,17 @@ async function submitForm(event) {
             alert(response.data.message);
             buscaEstoque();
             document.getElementById("form-cadastrob").reset();
-        }catch(error){
+        } catch (error) {
             console.error('Erro ao enviar os dados:', error);
             alert(error.message);
         }
-    }else{
+    } else {
         window.location.href = '/login'
     }
 }
 
-function submitVenda(){
-    event.preventDefault(); 
+function submitVenda() {
+    event.preventDefault();
     document.getElementById("form-venda").reset();
     alert('Venda confirmada!');
 }
--- routes/crud.js
+++ routes/crud.js
@@ -49,12 +49,52 @@ function authenticateToken(req, res, next) {
     }
   })
 
-  router.get('/estoque', authenticateToken, async (req,res) =>{
+  router.post('/estoque', authenticateToken, async (req,res) =>{
+    const {user_id} = req.body;
     try{
-      const response = await conn`SELECT * FROM bebidas`;
+      const response = await conn`SELECT * FROM bebidas WHERE user_id = ${user_id} ORDER BY nome ASC`;
       res.json(response);
     }catch(error){
       console.error('Erro ao pesquisar bebidas:', error);
+      res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    }
+  })
+
+  router.post('/estoque-id', authenticateToken, async(req, res) => {
+    const { id } = req.body;
+    try{
+      const response = await conn`SELECT * FROM bebidas WHERE id = ${id}`;
+      res.json(response);
+    }catch(error){
+      console.error('Erro ao pesquisar bebida:', error);
+      res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    }
+  })
+
+  router.post('/alterar-estoque',authenticateToken, async(req,res) => {
+    const { id, quantidade_caixas, unidades_por_caixa, valor_gasto_por_caixa, valor_venda_por_unidade } =req.body;
+    const total_unidades = quantidade_caixas * unidades_por_caixa;
+    const custo_por_unidade = valor_gasto_por_caixa / unidades_por_caixa;
+    try {
+      await conn`UPDATE bebidas
+                  SET custo_por_unidade = ${custo_por_unidade},
+                  total_unidades = total_unidades + ${total_unidades},
+                  valor_venda_por_unidade = ${valor_venda_por_unidade}
+                  WHERE id = ${id}`;
+      res.status(200).json({ message: 'Estoque atualizado com sucesso' });
+    }catch(error){
+      console.error('Erro ao alterar dados do estoque:', error);
+      res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    }
+  })
+
+  router.post('/excluir-bebida', authenticateToken, async(req,res) => {
+    const { id } =req.body;
+    try{
+      await conn`DELETE FROM bebidas WHERE id = ${id}`;
+      res.status(200).json({ message: 'Produto excluído com sucesso' });
+    }catch(error){
+      console.error('Erro ao alterar dados do estoque:', error);
       res.status(500).json({ error: 'Erro ao processar a solicitação' });
     }
   })
